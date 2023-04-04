d3e91f7359be9504b300b61ef7d1f43975ce90f9
arthurjf <arthur.310100@hotmail.com>
2023-04-04 13:00:58 -0300

    Questao03 com melhor legibilidade

--- questao03.py
+++ questao03.py
@@ -8,38 +8,52 @@
 #   	b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 
 import json
-  
-file = open('dados.json')
-  
-faturas = json.load(file)
 
-faturas.sort(key=lambda x: x['valor'], reverse=True)
+def carregarJson(nomeArquivo):
+  with open(nomeArquivo) as arquivoJson:
+      faturamentos = json.load(arquivoJson)
+      return faturamentos
+  
+def getMediaFaturamentoMes(faturas):
+    faturaTotal = 0.0
+    diasFaturados = 0
+    for tempFatura in faturas:
+        faturaValor = float(tempFatura['valor'])
+        if faturaValor > 0:
+            faturaTotal += faturaValor
+            diasFaturados += 1
+    mediaFatura = faturaTotal / diasFaturados
+    return mediaFatura
 
-file.close()
+def getNumeroDiasFaturamentoAcimaMedia(faturas, mediaFaturamento):
+    diasFaturaAcimaMedia = 0
+    for tempFatura in faturas:
+        faturaValor = float(tempFatura['valor'])
+        if faturaValor > mediaFaturamento:
+            diasFaturaAcimaMedia += 1
+        else:
+            return diasFaturaAcimaMedia
+    return 0
+
+def getMenorFaturamento(faturas):
+    qtdFaturamentos = len(faturamentos) - 1
+    return faturas[qtdFaturamentos]
+
+def getMaiorFaturamento(faturas):
+    return faturas[0]
+  
+faturamentos = carregarJson("dados.json")
+faturamentos.sort(key=lambda x: x['valor'], reverse=True)
 
 #   	• O menor valor de faturamento ocorrido em um dia do mês;
-menorFatura = faturas[len(faturas)-1]
+menorFatura = getMenorFaturamento(faturamentos)
 print("Menor faturamento: {0} no dia {1}".format(menorFatura['valor'], menorFatura['dia']))
 
 #   	• O maior valor de faturamento ocorrido em um dia do mês;
-maiorFatura = faturas[0]
+maiorFatura = getMaiorFaturamento(faturamentos)
 print("Maior faturamento: {0} no dia {1}".format(maiorFatura['valor'], maiorFatura['dia']))
 
-faturaTotal = 0.0
-diasFaturados = 0
-for tempFatura in faturas:
-    faturaValor = float(tempFatura['valor'])
-    if faturaValor > 0:
-        faturaTotal += faturaValor
-        diasFaturados += 1
-mediaFatura = faturaTotal / diasFaturados
+mediaFatura = getMediaFaturamentoMes(faturamentos)
 
 #   	• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
-diasFaturaAcimaMedia = 0
-for tempFatura in faturas:
-    faturaValor = float(tempFatura['valor'])
-    if faturaValor > mediaFatura:
-        diasFaturaAcimaMedia += 1
-    else:
-        print("Dias de faturamento acima da média mensal: {0}".format(diasFaturaAcimaMedia))
-        exit()
+print("Dias de faturamento acima da média mensal: {0}".format(getNumeroDiasFaturamentoAcimaMedia(faturamentos, mediaFatura)))
